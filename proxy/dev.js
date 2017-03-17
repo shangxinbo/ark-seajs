2b15203b2b7080a2869323c60689042627c703af
shangxinbo <314911714@qq.com>
2017-03-17 14:38:08 +0800

    调试接口

--- proxy/dev.js
+++ proxy/dev.js
@@ -14,6 +14,17 @@ module.exports = app => {
                     please add a module to '${req.path}'
                 `)
             }
+        }else if(req.url.indexOf('/api')>=0){  //直接访问接口
+            try {
+                const data = require('..' + req.path)
+                res.send(data(req, res))
+            } catch (e) {
+                res.send(`
+                    Error: API is undefined.
+                    If you want to use this api,
+                    please add a module to '${req.path}'
+                `)
+            }
         } else {
             res.render(path.join(__dirname, '../views', req.path))
         }
